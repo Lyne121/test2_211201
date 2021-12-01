d192004203393e030715bcef309f7fc7b9911e8d
leejiwon <irne000805@gmail.com>
2021-12-01 16:32:29 +0900

    Initial commit & Finish

---
+++ app/src/main/java/com/practicejw/test2_211201/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.practicejw.test2_211201
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
